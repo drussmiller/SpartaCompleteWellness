b2be6df19ac583c1da1980b9aab4ff3e6a879d14
drussmiller2 <39881282-drussmiller2@users.noreply.replit.com>
2025-02-23 06:59:42 +0000

    User checkpoint: Increase logo size in UI

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -1,14 +1,13 @@
-
-import React from 'react';
+import React from "react";
 
 export function Logo() {
   return (
-    <div className="relative h-12 w-12">
-      <img 
+    <div className="relative h-16 w-16">
+      <img
         src="/Sparta_Logo.jpg"
         alt="Sparta Logo"
         className="h-full w-full object-contain"
-        style={{ imageRendering: 'crisp-edges' }}
+        style={{ imageRendering: "crisp-edges" }}
       />
     </div>
   );
