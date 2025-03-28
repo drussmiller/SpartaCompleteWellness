5836a4fd41f69769a0a59f029d11fd9f80ef1b86
drussmiller <drussmiller@yahoo.com>
2025-03-28 21:04:12 +0000

    restored to remove page height adjustments

--- client/src/components/messaging/message-slide-card.tsx
+++ client/src/components/messaging/message-slide-card.tsx
@@ -325,9 +325,9 @@ export function MessageSlideCard() {
             </ScrollArea>
           ) : (
             // Messages View
-            <div className="flex flex-col h-[calc(100vh-15rem)]">
+            <div className="flex flex-col h-[calc(100vh-5rem)]">
               {/* Messages List */}
-              <ScrollArea className="flex-1 p-4 overflow-y-auto">
+              <ScrollArea className="flex-1 p-4">
                 <div className="space-y-4">
                   {messages.map((message) => (
                     <div
