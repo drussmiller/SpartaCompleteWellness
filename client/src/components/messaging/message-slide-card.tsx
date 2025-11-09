15029f378587036f03134d6330e5628186e3cae4
drussmiller <drussmiller@yahoo.com>
2025-11-09 05:31:06 +0000

    Restored to '062f5f379e13d8eeb823d7db4777f1666171ef37'
    Replit-Restored-To: 062f5f379e13d8eeb823d7db4777f1666171ef37

--- client/src/components/messaging/message-slide-card.tsx
+++ client/src/components/messaging/message-slide-card.tsx
@@ -466,11 +466,10 @@ export function MessageSlideCard() {
       {isOpen && createPortal(
         <div
         ref={cardRef}
-        className="fixed left-0 right-0 top-0 bg-white z-[2147483647] flex flex-col"
+        className="fixed left-0 right-0 bottom-0 bg-white z-[2147483647] flex flex-col"
         style={{
-          height: '100vh',
-          maxHeight: '100vh',
-          paddingBottom: keyboardHeight > 0 ? `${keyboardHeight}px` : '0px',
+          height: keyboardHeight > 0 ? `calc(100vh - ${keyboardHeight}px)` : '100vh',
+          maxHeight: keyboardHeight > 0 ? `calc(100vh - ${keyboardHeight}px)` : '100vh',
           touchAction: 'none'
         }}
         onTouchStart={handleTouchStart}
